fb7d334b2415ca074670c9177573a2289dae3516
xbrainnet <148790283+xbrainnet@users.noreply.github.com>
2025-06-15 00:45:43 +0800

    Create layers.py

---
+++ models/dgcnn/layers.py
@@ -0,0 +1,36 @@
+import torch
+from torch import nn
+
+class GraphConvolution(nn.Module):
+
+    def __init__(self, in_channels, out_channels, bias=False):
+
+        super(GraphConvolution, self).__init__()
+
+        self.in_channels = in_channels
+        self.out_channels = out_channels
+        self.weight = nn.Parameter(torch.FloatTensor(in_channels, out_channels).cuda())
+        nn.init.xavier_normal_(self.weight)
+        self.bias = None
+        if bias:
+            self.bias = nn.Parameter(torch.FloatTensor(out_channels).cuda())
+            nn.init.zeros_(self.bias)
+
+    def forward(self, x, adj):
+        out = torch.matmul(adj, x)
+        out = torch.matmul(out, self.weight)
+        if self.bias is not None:
+            return out + self.bias
+        else:
+            return out
+
+class Linear(nn.Module):
+    def __init__(self, in_channels, out_channels, bias=True):
+        super(Linear, self).__init__()
+        self.linear = nn.Linear(in_channels, out_channels, bias=bias)
+        nn.init.xavier_normal_(self.linear.weight)
+        if bias:
+            nn.init.zeros_(self.linear.bias)
+
+    def forward(self, inputs):
+        return self.linear(inputs)
